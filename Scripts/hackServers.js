7e90911a55ef15ac2eabf7fbfbd8258c1f9f5dd6
BoerBart <b.denboer@hotmail.com>
2023-03-04 20:15:23 +0100

    Updated hackServers.js
    Applied suggested change by SonarLint

--- Scripts/hackServers.js
+++ Scripts/hackServers.js
@@ -5,30 +5,30 @@ export async function main(ns) {
 	const target = ns.scan(); //scan all targets that can be found from home. Won't go deeper than 1 level.
 	const playerPortsAccess = 5; //Amount of ports that can be hacked by user by using the software for it.
 
-	for (let i = 0; i < target.length; ++i) {
+	for (const element of target) {
 
-		if (ns.getServerRequiredHackingLevel(target[i]) <= ns.getHackingLevel() && ns.getServerNumPortsRequired(target[i]) <= playerPortsAccess) {
+		if (ns.getServerRequiredHackingLevel(element) <= ns.getHackingLevel() && ns.getServerNumPortsRequired(element) <= playerPortsAccess) {
 			// if ports to hack is eq/higher than 1, run brutessh.
-			if (ns.getServerNumPortsRequired(target[i]) >= 1 && ns.getServerNumPortsRequired(target[i]) <= playerPortsAccess) {
-				ns.brutessh(target[i]);
+			if (ns.getServerNumPortsRequired(element) >= 1 && ns.getServerNumPortsRequired(element) <= playerPortsAccess) {
+				ns.brutessh(element);
 				await ns.sleep(1000);
 			}
 			// if ports to hack is eq/higher than 2, run ftpcrack.
-			if (ns.getServerNumPortsRequired(target[i]) >= 2 && ns.getServerNumPortsRequired(target[i]) <= playerPortsAccess) {
-				ns.ftpcrack(target[i]);
+			if (ns.getServerNumPortsRequired(element) >= 2 && ns.getServerNumPortsRequired(element) <= playerPortsAccess) {
+				ns.ftpcrack(element);
 				await ns.sleep(1000);
 			}
 
-			if (!ns.hasRootAccess(target[i])) {
-				ns.nuke(target[i]);
+			if (!ns.hasRootAccess(element)) {
+				ns.nuke(element);
 			}
 			// if server has root access, calculate how much threads the script can use, rm the old script, deploy new script and run it with max threads.
-			if (ns.hasRootAccess(target[i])) {
-				let maxThreads = (parseInt(ns.getServerMaxRam(target[i]) / ns.getScriptRam("moneyScript.script")));
-				ns.kill("moneyScript.script", target[i]);
-				ns.rm("moneyScript.script", target[i]);
-				ns.scp("moneyScript.script", target[i], "home");
-				ns.exec("moneyScript.script", target[i], maxThreads);
+			if (ns.hasRootAccess(element)) {
+				let maxThreads = (parseInt(ns.getServerMaxRam(element) / ns.getScriptRam("moneyScript.script")));
+				ns.kill("moneyScript.script", element);
+				ns.rm("moneyScript.script", element);
+				ns.scp("moneyScript.script", element, "home");
+				ns.exec("moneyScript.script", element, maxThreads);
 			}
 		}
 	}
